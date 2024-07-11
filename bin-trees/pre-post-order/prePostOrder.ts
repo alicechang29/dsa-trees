bec09f3ab5370d0864771b6c01e016f99a2d493e
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-07-11 14:51:23 -0700

    bin preOrder

--- bin-trees/pre-post-order/prePostOrder.ts
+++ bin-trees/pre-post-order/prePostOrder.ts
@@ -1,10 +1,41 @@
 import { BNodeNum } from "../common/bintree";
+import { Stack } from "../common/stack";
+/*
+Pre-order: parent first then child from left to right
+
+- start at root node
+- add node to the stack
+- check left
+- add left node to the stack
+- check children
+- add left... continue
+- check right
+
+- do this until
+
+Stack
+*/
 
 /** dfsPreOrder(): Traverse from the invoking node using pre-order DFS.
  * Returns an array of values of visited nodes. */
 
 function preOrder(node: BNodeNum | null): number[] {
-  return [42];
+  //base
+  if (node === null) return [];
+
+  //stack: bottom: A, B,D,E,F, C, G :top
+
+  //NOTE: need to add initial node to the result
+  const result: number[] = [node.val]; //A, B
+  const lNode = node.lnode; // B, D
+  const rNode = node.rnode; // C, E
+
+  // preOrder returns an array
+  // to combine result with any new results, spread out the return
+  result.push(...preOrder(lNode));
+  result.push(...preOrder(rNode));
+
+  return result; //[A, B, D, ]
 }
 
 
--- trees/pre-post-order/prePostOrder.ts
+++ trees/pre-post-order/prePostOrder.ts
@@ -1,6 +1,37 @@
 import { TreeNodeNum } from "../common/tree";
 import { Stack } from "../common/stack";
 
+/*
+Pre-order -> do yourself first before children
+Using recursion:
+- define base case: if(!node) return [];
+-- base case needs to be truthful (traversal from empty node is empty array)
+
+const out = [node.val]
+- make an array first that contains "you"
+
+for (const c of node.children) {
+//
+  out.push(...preOrder(c))
+
+}
+*/
+
+function preOrderR(node: TreeNodeNum | null): number[] {
+  //define base case:
+  if (!node) return [];
+  //base case needs to be truthful (traversal from empty node is empty array)
+
+  const out = [node.val];
+  //make an array first that contains "you";
+
+  for (const c of node.children) {
+    //pushing the result of asking the same question to my children
+    out.push(...preOrderR(c));
+  }
+  return out;
+}
+
 /** dfsPreOrder(): Traverse from the invoking node using pre-order DFS.
  * Returns an array of values of visited nodes. */
 
@@ -14,10 +45,12 @@ function preOrder(node: TreeNodeNum | null): number[] {
 
   while (!toVisit.isEmpty()) {
 
-    let currNode = toVisit.pop(); // 1
+    let currNode = toVisit.pop();
+    //add myself to output first before doing anything to children
     output.push(currNode.val);
 
     for (let i = currNode.children.length - 1; i >= 0; i--) {
+      //put children into stack
       toVisit.push(currNode.children[i]);
 
     }
@@ -31,14 +64,20 @@ function preOrder(node: TreeNodeNum | null): number[] {
  * Returns an array of values of visited nodes. */
 
 function postOrder(node: TreeNodeNum | null): number[] {
+
   if (node === null) return [];
+  let output: number[] = [];
+  //post order returns an array
 
-  const toVisit = new Stack<TreeNodeNum>([node]);
-  // BOTTOM - 1, 2, 3, 4, 5, 6, 7, 8 - TOP
+  //traverse all children
+  for (const child of node.children) {
+    // push the value into the array
+    output.push(...postOrder(child));
+    console.log({ output });
+  }
 
-  let output: number[] = []; //1
-
-
+  //insert the root last
+  output.push(node.val);
 
   return output;
 }
