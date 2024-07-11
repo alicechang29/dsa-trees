aab183eaf4dbdcecdd8bae0970b17f0a32fb56d2
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-07-10 18:05:30 -0700

    prePost

--- trees/pre-post-order/prePostOrder.ts
+++ trees/pre-post-order/prePostOrder.ts
@@ -1,10 +1,29 @@
 import { TreeNodeNum } from "../common/tree";
+import { Stack } from "../common/stack";
 
 /** dfsPreOrder(): Traverse from the invoking node using pre-order DFS.
  * Returns an array of values of visited nodes. */
 
 function preOrder(node: TreeNodeNum | null): number[] {
-  return [42];
+  if (node === null) return [];
+
+  const toVisit = new Stack<TreeNodeNum>([node]);
+  // BOTTOM - 1, 2, 3, 4, 5, 6, 7, 8 - TOP
+
+  let output: number[] = []; //1
+
+  while (!toVisit.isEmpty()) {
+
+    let currNode = toVisit.pop(); // 1
+    output.push(currNode.val);
+
+    for (let i = currNode.children.length - 1; i >= 0; i--) {
+      toVisit.push(currNode.children[i]);
+
+    }
+  }
+
+  return output;
 }
 
 
@@ -12,7 +31,16 @@ function preOrder(node: TreeNodeNum | null): number[] {
  * Returns an array of values of visited nodes. */
 
 function postOrder(node: TreeNodeNum | null): number[] {
-  return [42];
+  if (node === null) return [];
+
+  const toVisit = new Stack<TreeNodeNum>([node]);
+  // BOTTOM - 1, 2, 3, 4, 5, 6, 7, 8 - TOP
+
+  let output: number[] = []; //1
+
+
+
+  return output;
 }
 
 export { preOrder, postOrder };
